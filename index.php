c6ba445327b4bd7a5ecf2927eb2c583d43e1352d
James Todd <james9005@hotmail.co.uk>
2015-10-19 14:58:23 +0100

    Changed Test

--- index.php
+++ index.php
@@ -5,8 +5,14 @@
 <body>
 <p>
     <?php
-            echo "Hello World"; 
-          ?>
+    $fav_bands = array();
+    array_push($fav_bands, "Katy Perry");
+    array_push($fav_bands, "The Peapods");
+    array_push($fav_bands, "Nickelback");
+
+    print count($fav_bands);
+    ?>
+
 </p>
 </body>
 </html>
